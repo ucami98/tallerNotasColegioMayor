9d6801f9d3d2075e42b17c39495913a4a184916b
Camila <ucami98@gmail.com>
2021-10-20 17:44:03 -0500

    last update

--- assets/consultardata.js
+++ assets/consultardata.js
@@ -1,4 +1,3 @@
-
 document.getElementById("ConsultarBtn").addEventListener("click", function () {
   var inputId = document.getElementById("ConsultID").value;
   var dataConsultada = localStorage.getItem(inputId);
@@ -8,17 +7,19 @@ document.getElementById("ConsultarBtn").addEventListener("click", function () {
     alert("Ese dato no existe en el localStorage");
   } else {
     var pElement = document.getElementById("data");
-    var FinalNote = ((parseInt(array[1]) + parseInt(array[2]) + parseInt(array[3])) / 3);
+    var FinalNote =
+      (parseInt(array[1]) + parseInt(array[2]) + parseInt(array[3])) / 3;
     if (FinalNote >= 3) {
       var result = "ha aprobado";
     } else {
       var result = "no ha aprobado";
     }
-    pElement.innerHTML = "Nombre: "+array[0] + "<br>";
+    pElement.innerHTML = "Nombre: " + array[0] + "<br>";
     pElement.innerHTML = pElement.innerHTML + "Nota 1: " + array[1] + "<br>";
     pElement.innerHTML = pElement.innerHTML + "Nota 2: " + array[2] + "<br>";
     pElement.innerHTML = pElement.innerHTML + "Nota 3: " + array[3] + "<br>";
-    pElement.innerHTML = pElement.innerHTML + "Su nota final es: " + FinalNote.toFixed(2) + "<br>" ;
+    pElement.innerHTML =
+      pElement.innerHTML + "Su nota final es: " + FinalNote.toFixed(2) + "<br>";
     pElement.innerHTML = pElement.innerHTML + " Usted " + result + "<br>";
   }
 });
